1caaa2e14a69bf0e4d966fc5820a384db67c9d06
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-09-09 16:58:25 -0300

    CC, refatored Login function

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,37 +2,54 @@ import React, { useState, useContext } from 'react';
 import { Link } from 'react-router-dom';
 import RecipeContext from '../context/RecipeContext';
 
+function inputEmail(handleEmailChange) {
+  return (
+    <input
+      type="email"
+      placeholder="Email"
+      data-testid="email-input"
+      name="email"
+      onChange={(e) => handleEmailChange(e)}
+    />
+  );
+}
+
+function inputPassword(handlePasswordChange) {
+  return (
+    <input
+      type="password"
+      placeholder="Senha"
+      data-testid="password-input"
+      name="password"
+      onChange={(e) => handlePasswordChange(e)}
+    />
+  );
+}
+
+// ref for regex: https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail .
+
 function Login() {
   const [checkedEmail, setCheckedEmail] = useState(false);
   const [checkedPassword, setCheckedPassword] = useState(false);
   const { email, setEmail, setPassword } = useContext(RecipeContext);
 
+  const checkEmail = (emailTested) => {
+    const regexEmail = /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/;
+    if (regexEmail.test(emailTested) === true) setCheckedEmail(true);
+  };
+  const checkPassword = (passwordTested) => {
+    if (passwordTested.length > 6) setCheckedPassword(true);
+  };
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
     checkEmail(e.target.value);
   };
-
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
     checkPassword(e.target.value);
   };
 
-  const checkEmail = (emailTested) => {
-    const regexEmail = /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/;
-    if (regexEmail.test(emailTested) === true) {
-      setCheckedEmail(true);
-    }
-  };
-  // ref for regex: https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail .
-
-  const checkPassword = (passwordTested) => {
-    if (passwordTested.length > 6) {
-      setCheckedPassword(true);
-    }
-  };
-
   const storage = () => {
-    // still no fetch here.
     localStorage.setItem('mealsToken', 1);
     localStorage.setItem('cocktailsToken', 1);
     localStorage.setItem('email', JSON.stringify({ email }));
@@ -41,20 +58,8 @@ function Login() {
   return (
     <div data-testid="">
       <h1>Login</h1>
-      <input
-        type="email"
-        placeholder="Email"
-        data-testid="email-input"
-        name="email"
-        onChange={(e) => handleEmailChange(e)}
-      />
-      <input
-        type="password"
-        placeholder="Senha"
-        data-testid="password-input"
-        name="password"
-        onChange={(e) => handlePasswordChange(e)}
-      />
+      {inputEmail(handleEmailChange)}
+      {inputPassword(handlePasswordChange)}
       <Link to="/comidas">
         <button
           type="button"
