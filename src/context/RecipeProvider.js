84a42a08ecb443ac1f4ad93ec7f09098b954f649
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-09-09 12:53:45 -0300

    Login added data testid, all html, onchange

--- src/context/RecipeProvider.js
+++ src/context/RecipeProvider.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import RecipeContext from './RecipeContext';
 
 const RecipeProvider = ({ children }) => {
-  const context = {};
+  const [user, setUser] = useState({
+    userData: {
+      email : '',
+      password: '',
+    },
+  });
+  const [checked, setChecked] = useState(false);
+
+  const context = {
+    user,
+    email: user.userData.email,
+    password: user.userData.password,
+    setUser,
+    checked,
+    setChecked,
+  };
+
   return (
     <RecipeContext.Provider value={context}>
       {children}
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,65 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import RecipeContext from '../context/RecipeContext';
+
+// function handleEmailChange(e) {
+//   const { setUser } = useContext(RecipeContext);
+//   setUser({ userData: { email: e.target.value } });
+//   checkForm();
+// }
+
+// function handlePasswordChange(e) {
+//   const { setUser } = useContext(RecipeContext);
+//   setUser({ userData: { password: e.target.value } });
+//   checkForm();
+// }
+
+// function checkForm() {
+// const { email, password, setChecked } = useContext(RecipeContext);
+// if ((password.length > 6) && (/^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/.test(email) === true)) {
+// setChecked(true);
+// }
+// }
+// problema: useContext recusado quando nao for dentro de componente
+
+function provokeApis(emailInput) {
+  // 1. fetch Tokens
+  // apifood
+  // apidrink
+  // make both in services and after token
+  // 2. diverse storage operations
+  // localStorage.setItem('mealsToken', ...)
+  // localStorage.setItem('cocktailsToken', ...)
+  // localStorage.setItem('email', email do input)
+}
 
 function Login() {
+  const { setUser, checked } = useContext(RecipeContext);
   return (
-    <div>
-      Login Page
+    <div data-testid="">
+      <h1>Login</h1>
+      <input
+        type="email"
+        placeholder="Email"
+        data-testid="email-input"
+        name="email"
+        // onChange={(e) => handleEmailChange(e) }
+        onChange={(e) => setUser({ userData: { email: e.target.value } })}
+        // problema para ambos onChange: também chamar o checkForm
+      />
+      <input
+        type="password"
+        placeholder="Senha"
+        data-testid="password-input"
+        name="password"
+        // onChange={(e) => handlePasswordChange(e) }
+        onChange={(e) => setUser({ userData: { password: e.target.value } })}
+      />
+      <Link to="/comidas">
+        <button type="button" data-testid="login-submit-btn" disabled={!checked} onClick={() => provokeApis()}>
+          Entrar
+        </button>
+      </Link>
     </div>
   );
 }
