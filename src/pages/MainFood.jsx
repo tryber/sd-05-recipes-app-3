0553391ef10a504873559ce130bda52294a4aff3
Mau <mauricio.sdjr@gmail.com>
2020-09-10 13:44:40 -0300

    testes de api

--- src/pages/MainFood.jsx
+++ src/pages/MainFood.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React,{ useEffect, useContext } from 'react';
 import Header from '../components/Header';
+import { foodIngredientAPI } from '../service/apis';
+import RecipeContext from '../context/RecipeContext';
 
 function MainFood() {
+  const { data, setData, fetching, setFetching } = useContext(RecipeContext);
+  useEffect(() => {
+    setFetching(true);
+    foodIngredientAPI("chicken").then((response) => setData(response.meals));
+    setFetching(false);
+  }, []);
   return (
     <div>
       <Header />
       <p>MainFood Page</p>
+      {data.map((item) =>
+      <div> 
+        <h2>{item.strMeal}</h2>
+        <img src={item.strMealThumb} width="200px" />
+      </div>
+      )}
+      { console.log(data)}
     </div>
   );
 }
