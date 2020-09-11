f2caa88785fd40ad3277bb7d19a4b5ac1d9b9e36
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-09-11 17:49:15 -0300

    explore drinks and foods ok except for surprise

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ import DrinkDetails from './pages/FoodDetails';
 // import DrinkProcess from './pages/DrinkProcess';
 import Explore from './pages/Explore';
 import ExploreDrink from './pages/ExploreDrink';
-// import ExploreDrinkIngredient from './pages/ExploreDrinkIngredient';
+import ExploreDrinkIngredient from './pages/ExploreDrinkIngredient';
 import ExploreFood from './pages/ExploreFood';
-// import ExploreFoodIngredient from './pages/ExploreFoodIngredient';
-// import ExploreFoodOrigin from './pages/ExploreFoodOrigin';
+import ExploreFoodIngredient from './pages/ExploreFoodIngredient';
+import ExploreFoodOrigin from './pages/ExploreFoodOrigin';
 import DoneRecipes from './pages/DoneRecipes';
 import FavoriteRecipes from './pages/FavoriteRecipes';
 import Profile from './pages/Profile';
@@ -42,9 +42,9 @@ render={(props) => <DrinkProcess {...props} />} /> */}
         <Route exact path="/explorar" component={Explore} />
         <Route exact path="/explorar/comidas" component={ExploreFood} />
         <Route exact path="/explorar/bebidas" component={ExploreDrink} />
-        {/* <Route exact path="/explorar/comidas/ingredientes" component={ExploreFoodIngredient} />
+        <Route exact path="/explorar/comidas/ingredientes" component={ExploreFoodIngredient} />
         <Route exact path="/explorar/bebidas/ingredientes" component={ExploreDrinkIngredient} />
-        <Route exact path="/explorar/comidas/area component={ExploreFoodOrigin} /> */}
+        <Route exact path="/explorar/comidas/area" component={ExploreFoodOrigin} />
      
         <Route path="/perfil" component={Profile} />
         <Route path="/receitas-feitas" component={DoneRecipes} />
--- src/pages/ExploreDrink.jsx
+++ src/pages/ExploreDrink.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+// import { Redirect } from 'react-router-dom';
 import Footer from '../components/Footer';
+import Header from '../components/Header';
 
 function ExploreDrink() {
+  // const randomRecipeDetail = () => {
+    // "a rota deve mudar para a tela de detalhes de uma receita,
+    // que deve ser escolhida de forma aleatória através da API."
+    // ou seja fazer fetch para receber um id avulso
+    // e um Redirect:
+    // <Redirect to="/comidas/{randomId}" />
+  // };
   return (
     <div>
-      Explore Drink
+      <Header title="Explorar comidas" />
+      <Link to="/explorar/bebidas/ingredientes">
+        <button type="button" data-testid="explore-by-ingredient">
+          Por Ingredientes
+        </button>
+      </Link>
+      <button
+        type="button"
+        data-testid="explore-surprise"
+        // onClick={() => randomRecipeDetail()}
+      >
+        Me Surpreenda!
+      </button>
       <Footer />
     </div>
   );
