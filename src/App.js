4570577f155dbd47a3d38ddcd4360e2b380e34b9
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-09-11 17:23:00 -0300

    made explore page with link no explore food and drink

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import FoodDetails from './pages/FoodDetails';
 import DrinkDetails from './pages/FoodDetails';
 // import FoodProcess from './pages/FoodProcess';
 // import DrinkProcess from './pages/DrinkProcess';
-// import Explore from './pages/Explore';
-// import ExploreDrink from './pages/ExploreDrink';
+import Explore from './pages/Explore';
+import ExploreDrink from './pages/ExploreDrink';
 // import ExploreDrinkIngredient from './pages/ExploreDrinkIngredient';
-// import ExploreFood from './pages/ExploreFood';
+import ExploreFood from './pages/ExploreFood';
 // import ExploreFoodIngredient from './pages/ExploreFoodIngredient';
 // import ExploreFoodOrigin from './pages/ExploreFoodOrigin';
 import DoneRecipes from './pages/DoneRecipes';
@@ -38,14 +38,14 @@ function App() {
         {/* <Route exact path="/comidas/:idRecipe/in-progress"
 render={(props) => <FoodProcess {...props} />} />
 <Route exact path="/bebidas/:idRecipe/in-progress"
-render={(props) => <DrinkProcess {...props} />} />
+render={(props) => <DrinkProcess {...props} />} /> */}
         <Route exact path="/explorar" component={Explore} />
         <Route exact path="/explorar/comidas" component={ExploreFood} />
         <Route exact path="/explorar/bebidas" component={ExploreDrink} />
-        <Route exact path="/explorar/comidas/ingredientes" component={ExploreFoodIngredient} />
+        {/* <Route exact path="/explorar/comidas/ingredientes" component={ExploreFoodIngredient} />
         <Route exact path="/explorar/bebidas/ingredientes" component={ExploreDrinkIngredient} />
-        <Route exact path="/explorar/comidas/area component={ExploreFoodOrigin} />
-      */}
+        <Route exact path="/explorar/comidas/area component={ExploreFoodOrigin} /> */}
+     
         <Route path="/perfil" component={Profile} />
         <Route path="/receitas-feitas" component={DoneRecipes} />
         <Route path="/receitas-favoritas" component={FavoriteRecipes} />
