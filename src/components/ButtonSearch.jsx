0d449989c645f6cc1564efe92d6d86c9c77d8e3f
Mau <mauricio.sdjr@gmail.com>
2020-09-11 19:50:55 -0300

    codeclimate

--- src/components/ButtonSearch.jsx
+++ src/components/ButtonSearch.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function ButtonSearch(props) {
-  const { onClick } = props;
   return (
     <button
       data-testid="exec-search-btn"
       type="button"
-      onClick={onClick}
     >
     Buscar
     </button>
@@ -15,7 +13,3 @@ function ButtonSearch(props) {
 }
 
 export default ButtonSearch;
-
-ButtonSearch.protoTypes = {
-  onClick: PropTypes.function.isRequired,
-};
