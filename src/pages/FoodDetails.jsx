02732cf28da040dd42b84326cd4743eacf3272f3
Larissa Palombo <larissampalombo@gmail.com>
2020-09-15 17:59:02 -0300

    esqueleto Ongoing recipe

--- src/pages/FoodDetails.jsx
+++ src/pages/FoodDetails.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useContext, useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { lookUpIdMeal } from '../service/apis';
 import RecipeContext from '../context/RecipeContext';
-import '../css/Details';
+import '../css/Details.css';
 import { whiteHeartIcon } from '../images';
 import { blackHeartIcon } from '../images';
 import { shareIcon } from '../images';
@@ -13,8 +13,8 @@ function FoodDetails(props) {
   const [favorite, setFavorite] = useState(false);
   const handleFavorite = () => {
     setFavorite(!favorite);
-  }
-    /* const receitaFavoritada = 
+  };
+  /* const receitaFavoritada = 
       { id, type, area, category, alcoholicOrNot, name, image }
     localStorage.setItem('favoriteRecipes', JSON.stringify({ receitaFavoritada })); */
   const { idRecipe } = props.match.params;
@@ -25,60 +25,81 @@ function FoodDetails(props) {
   const ingredientValue = (ingredients);
   console.log(ingredientValue); */
   const recipeGetter = (ingredient) => {
-    const items = Object.keys(ingredient).filter((keysIngridients) => keysIngridients.includes('strIngredient')).filter(value => ingredient[value] !== '');
-    const quantities = Object.keys(ingredient).filter((qtIngridients) => qtIngridients.includes('strMeasure')).filter(value => ingredient[value] !== '');
+    const items = Object.keys(ingredient)
+      .filter((keysIngridients) => keysIngridients.includes('strIngredient'))
+      .filter((value) => ingredient[value] !== '');
+    const quantities = Object.keys(ingredient)
+      .filter((qtIngridients) => qtIngridients.includes('strMeasure'))
+      .filter((value) => ingredient[value] !== '');
     // construindo um array de ingredientes
-    const allIngredients = items.map(value => ingredient[value]);
+    const allIngredients = items.map((value) => ingredient[value]);
     // construindo um array de medidas dos ingredientes
-    const allMeasures = quantities.map(value => ingredient[value]);
+    const allMeasures = quantities.map((value) => ingredient[value]);
     return { allIngredients, allMeasures };
   };
-  const { allIngredients, allMeasures } = recipeGetter(details[0])
-// uma saida no console pra vc saber o que esta manipulado
-console.log(allIngredients, allMeasures);
-  
+  const { allIngredients, allMeasures } = recipeGetter(details[0]);
+  // uma saida no console pra vc saber o que esta manipulado
+  console.log(allIngredients, allMeasures);
+
   useEffect(() => {
     setFetching(true);
-    lookUpIdMeal(idRecipe).then((food) => setDetails(food.meals))
-    // (food.meals) ?
-    // setDetails(food.meals) : alert('Erro no servidor! tente novamente.'))
-    .catch((error) => alert('Algo inesperado aconteceingredients;u:', error));
+    lookUpIdMeal(idRecipe)
+      .then((food) => setDetails(food.meals))
+      // (food.meals) ?
+      // setDetails(food.meals) : alert('Erro no servidor! tente novamente.'))
+      .catch((error) => alert('Algo inesperado aconteceingredients;u:', error));
     setFetching(false);
   }, []);
 
   if (fetching) return <div>Loading...</div>;
-  return (idRecipe) ? (
+  return idRecipe ? (
     <div>
       FoodDetails Page
       <div className="card-recipe">
-        <img alt={strMeal} className="card-recipe-image" src={strMealThumb} data-testid="recipe-photo" />
+        <img
+          alt={strMeal}
+          className="card-recipe-image"
+          src={strMealThumb}
+          data-testid="recipe-photo"
+        />
         <div className="card-recipe-body">
-          <h3 className="card-recipe-name" data-testid="recipe-title">{strMeal}</h3>
+          <h3 className="card-recipe-name" data-testid="recipe-title">
+            {strMeal}
+          </h3>
           <p data-testid="recipe-category"></p>
-          <img src={favorite ? blackHeartIcon : whiteHeartIcon} alt="whiteHeart" data-testid="favorite-btn" onClick={() => handleFavorite() } />
-          <img src={shareIcon} alt="share" data-testid="share-btn" />
+          <button data-testid="favorite-btn">
+          <img
+            src={favorite ? blackHeartIcon : whiteHeartIcon}
+            alt="whiteHeart"
+            onClick={() => handleFavorite()}
+          />
+          </button>
+          <button data-testid="share-btn">
+          <img src={shareIcon} alt="share" />
+          </button>
           <h4>Ingredients</h4>
           <p data-testid="-ingredient-name-and-measure">
-          {allIngredients.map((ingredient, i) => (
-          <p>{ingredient} : {allMeasures[i]}</p>
-        ))}
+            {allIngredients.map((ingredient, i) => (
+              <p>
+                {ingredient} : {allMeasures[i]}
+              </p>
+            ))}
           </p>
           <h4>Instructions</h4>
           <p data-testid="instructions">{strInstructions}</p>
-          <video width="320" height="240" controls> 
+          <video width="320" height="240" controls>
             <source src={strYoutube} type="video/mp4" data-testid="video" />
           </video>
           <h4>Recomendadas</h4>
         </div>
-        <button data-testid="start-recipe-btn">Iniciar Receita</button>
-        <button data-testid="finish-recipe-btn">Finalizar Receita</button>
+        <Link to={`/comidas/${idRecipe}/in-progress`}>
+          <button data-testid="start-recipe-btn">Iniciar Receita</button>
+        </Link>
       </div>
     </div>
   ) : (
-    <Redirect to="/comidas/">
-      {alert('Não foi possível te surpreender desta vez!')}
-    </Redirect>
-    );
+    <Redirect to="/comidas/">{alert('Não foi possível te surpreender desta vez!')}</Redirect>
+  );
 }
 
 FoodDetails.propTypes = {
---
+++ src/pages/FoodProcess.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+// import '../css/explore.css';
+
+function FoodProcess() {
+  return (
+    <div>
+      <Header title="Food Process" />
+        Food Process
+      <Footer />
+    </div>
+  );
+}
+
+export default FoodProcess;
