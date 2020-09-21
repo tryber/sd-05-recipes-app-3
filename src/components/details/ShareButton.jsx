bb9810195754ec08e5616fa3a24aa538d37ac4d7
Mau <mauricio.sdjr@gmail.com>
2020-09-21 12:19:20 -0300

    arrumando pequenos erros

--- src/components/details/ShareButton.jsx
+++ src/components/details/ShareButton.jsx
@@ -1,31 +1,32 @@
 // Cesar me explicou
 import React from 'react';
 import PropTypes from 'prop-types';
-import { copy } from 'clipboard-copy';
-
+import copy from 'clipboard-copy';
 import shareIcon from '../../images/shareIcon.svg';
 
-function copyToClipboard(text) {
-  if (!text) copy(text);
-  return alert('Link copiado!');
+function copyToClipboard() {
+  document.getElementById('share-button').innerHTML = 'Link copiado!';
+  console.log(window.location.href)
+  return copy(window.location.href);
 }
+// honestidade acadêmica: ajuda do grupo 4
 
-function ShareButton(props) {
-  const { pathname } = props.url.location;
-  console.log(pathname);
-
-  return (
-    <div className="icon">
-      <input
-        style={{ textDecoration: 'none' }}
-        type="image"
-        src={shareIcon}
-        alt="Share Button"
-        data-testid="share-btn"
-        value={pathname}
-        onClick={({ target }) => copyToClipboard(target.value)}
-      />
-    </div>
+function ShareButton() {
+  /* const { pathname } = props.url.location;
+  console.log(pathname); */
+return (
+  <div className="icon">
+    <input
+      id="share-button"
+      style={{ textDecoration: 'none' }}
+      type="image"
+      src={shareIcon}
+      alt="Share Button"
+      data-testid="share-btn"
+      // value={pathname}
+      onClick={() => copyToClipboard()}
+    />
+  </div>
   );
 }
 
@@ -33,6 +34,6 @@ export default ShareButton;
 
 ShareButton.propTypes = {
   url: PropTypes.shape({
-    location: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
   }).isRequired,
 };
