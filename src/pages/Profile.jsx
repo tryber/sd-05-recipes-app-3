6c492bbe57b68fffa77a9b49eebf4826f01bdd2a
Juliette Beaudet <juliette.beaudet@sciencespo.fr>
2020-09-21 09:34:10 -0300

    CC

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useContext} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
