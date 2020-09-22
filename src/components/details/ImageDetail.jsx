9863bf89b7bded6e25e7d6c49b873ab7cbeba158
Mau <mauricio.sdjr@gmail.com>
2020-09-22 12:35:29 -0300

    codeclimate

--- src/components/details/ImageDetail.jsx
+++ src/components/details/ImageDetail.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import '../../css/details.css'
+import '../../css/details.css';
 
 class ImageDetail extends Component {
   // constructor(props){
