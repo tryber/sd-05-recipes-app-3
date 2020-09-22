919363604d4fbd4d31c3fbc012886c2d2ea96b48
Preto115 <preto115@gmail.com>
2020-09-21 23:14:30 -0300

    atualizando arquivo ShareButton com atualizacoes vindas da branch do Mauricio

--- src/components/details/ShareButton.jsx
+++ src/components/details/ShareButton.jsx
@@ -1,15 +1,24 @@
 // Cesar me explicou
 import React from 'react';
+import PropTypes from 'prop-types';
 import copy from 'clipboard-copy';
 import shareIcon from '../../images/shareIcon.svg';
 
-function copyToClipboard() {
+function copyToClipboard(url, idRecipe) {
   document.getElementById('share-button').innerHTML = 'Link copiado!';
-  return copy(window.location.href);
+  if (url.includes('comida')) {
+    copy(`http://localhost:3000/comidas/${idRecipe}`)
+      .then(() => alert('Link copiado'));
+  }
+  if (url.includes('bebida')) {
+    copy(`http://localhost:3000/bebidas/${idRecipe}`)
+      .then(() => alert('Link copiado'));
+  }
 }
 // honestidade acadêmica: ajuda do grupo 4
 
-function ShareButton() {
+function ShareButton(props) {
+  const { idRecipe, url } = props;
   /* const { pathname } = props.url.location;
   console.log(pathname); */
   return (
@@ -22,17 +31,17 @@ function ShareButton() {
         alt="Share Button"
         data-testid="share-btn"
         // value={pathname}
-        onClick={() => copyToClipboard()}
+        onClick={() => copyToClipboard(url, idRecipe)}
       />
     </div>
   );
 }
-// [data-testid="0-horizontal-share-btn"]
 
 export default ShareButton;
 
-// ShareButton.propTypes = {
-//   url: PropTypes.shape({
-//     location: PropTypes.string.isRequired,
-//   }).isRequired,
-// };
+ShareButton.propTypes = {
+  url: PropTypes.shape({
+    location: PropTypes.string.isRequired,
+  }).isRequired,
+  idRecipe: PropTypes.number.isRequired,
+};
