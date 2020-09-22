50ff893795878a2a116e824af46570aa569a6d87
Elisângelo <64562701+preto115@users.noreply.github.com>
2020-09-22 03:48:19 -0300

    Update App.js
    Eliminando espacos em branco

--- src/App.js
+++ src/App.js
@@ -38,7 +38,6 @@ function App() {
         <Route
           exact path="/bebidas/:idRecipe/in-progress"
           render={(props) => <OngoingRecipe {...props} type="bebidas" />}
-       
         />
         <Route exact path="/comidas" component={MainFood} />
         <Route exact path="/bebidas" component={MainDrink} />
